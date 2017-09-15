21b03d9f640bd4a72539c05fb19e03ebac50bbb3
yiduoke <yiduoke822@jhs185.org>
2017-09-14 23:09:48 -0400

    almost done; just need to convert strings to #s

--- anotherThing.py
+++ anotherThing.py
@@ -1,7 +1,39 @@
 f = open("occupations.csv","r") #opens file with name of "test.txt"
-string = f.read
+string = f.read()
 f.close
 
 d={}
+
+
+def function():
+    quoteCount = 0
+    character = 0
+    while character<len(string):
+        key = ""
+        value = ""
+        isValue = False
+        while character<len(string) and (string[character]!="\n"):
+            if (string[character]=='"'):
+                quoteCount+=1
+            if string[character]!='"' and string[character]!=",":
+                if isValue:
+                    value+=string[character]
+                else:
+                    key+=string[character]
+                #print (string[character]+ " "+str(character)+str(isValue))
+            elif string[character]==",":
+                if quoteCount%2==0:
+                    isValue=True
+                else:
+                    key+=string[character]
+                #print (string[character]+str(character)+str(isValue))
+            character+=1
+        d[key]=value       
+        character+=1
+    return d   
+
+print function()
+
+
 
 
